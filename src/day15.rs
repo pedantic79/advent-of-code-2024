17a517151a14cbde0d7dd6b0d4f1e32c41179940
Dan Nguyen <pedantic@gmail.com>
2024-12-15 14:19:28 -0600

    Day15: Use transmute to parse blocks

--- src/day15.rs
+++ src/day15.rs
@@ -1,11 +1,23 @@
-use std::fmt;
+use std::{fmt, mem::transmute, str::from_utf8_unchecked};
 
 use aoc_runner_derive::{aoc, aoc_generator};
 use pathfinding::{matrix::directions, utils::move_in_direction};
 
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+#[repr(u8)]
+enum Block {
+    Wall = b'#',
+    Box = b'O',
+    #[allow(dead_code)] // I don't know why but we never construct a Robot
+    Robot = b'@',
+    LBox = b'[',
+    RBox = b']',
+    Empty = b'.',
+}
+
 #[derive(Debug, PartialEq, Eq, Clone)]
 pub struct Input {
-    warehouse: Vec<Vec<u8>>,
+    warehouse: Vec<Vec<Block>>,
     robot_pos: (usize, usize),
     moves: Option<Vec<(isize, isize)>>,
     dim: (usize, usize),
@@ -25,8 +37,11 @@ fn dir_to_char(dir: (isize, isize)) -> char {
 impl fmt::Display for Input {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         for row in self.warehouse.iter() {
-            // SAFETY: We only use ASCII characters
-            writeln!(f, "{}", unsafe { std::str::from_utf8_unchecked(row) })?;
+            // SAFETY: from_utf8_unchecked: We only use ASCII characters
+            // SAFETY: Block has a repr(u8) and the values map to the u8 values
+            writeln!(f, "{}", unsafe {
+                from_utf8_unchecked(transmute::<&[Block], &[u8]>(row.as_slice()))
+            })?;
         }
 
         Ok(())
@@ -40,11 +55,11 @@ impl Input {
         self.moves.take().expect("moves already taken")
     }
 
-    fn warehouse_get(&self, (r, c): (usize, usize)) -> Option<u8> {
+    fn warehouse_get(&self, (r, c): (usize, usize)) -> Option<Block> {
         self.warehouse.get(r)?.get(c).copied()
     }
 
-    fn calculate_gps(&self, block: u8) -> usize {
+    fn calculate_gps(&self, block: Block) -> usize {
         self.warehouse
             .iter()
             .enumerate()
@@ -58,23 +73,23 @@ impl Input {
     }
 
     fn widen_warehouse(&self) -> Self {
-        let mut warehouse = vec![vec![b'.'; self.dim.1 * 2]; self.dim.0];
+        let mut warehouse = vec![vec![Block::Empty; self.dim.1 * 2]; self.dim.0];
 
         for (r, row) in self.warehouse.iter().enumerate() {
             for (c, block) in row.iter().enumerate() {
                 match block {
-                    b'#' => {
-                        warehouse[r][c * 2] = b'#';
-                        warehouse[r][c * 2 + 1] = b'#';
+                    Block::Wall => {
+                        warehouse[r][c * 2] = Block::Wall;
+                        warehouse[r][c * 2 + 1] = Block::Wall;
                     }
-                    b'O' => {
-                        warehouse[r][c * 2] = b'[';
-                        warehouse[r][c * 2 + 1] = b']';
+                    Block::Box => {
+                        warehouse[r][c * 2] = Block::LBox;
+                        warehouse[r][c * 2 + 1] = Block::RBox;
                     }
-                    b'@' => {
-                        warehouse[r][c * 2] = b'@';
+                    Block::Robot => {
+                        warehouse[r][c * 2] = Block::Robot;
                     }
-                    b'.' => {}
+                    Block::Empty => {}
                     _ => panic!("can't pass expanded input"),
                 }
             }
@@ -90,9 +105,9 @@ impl Input {
 
     /// Swaps the `pos` and `new_pos`, if `sim_only` is true, then we won't modify the warehouse.
     fn raw_move_block(&mut self, pos: (usize, usize), new_pos: (usize, usize), sim_only: bool) {
-        assert_ne!(self.warehouse_get(pos), Some(b'.'));
-        assert_ne!(self.warehouse_get(pos), Some(b'#'));
-        assert_eq!(self.warehouse_get(new_pos), Some(b'.'));
+        assert_ne!(self.warehouse_get(pos), Some(Block::Empty));
+        assert_ne!(self.warehouse_get(pos), Some(Block::Wall));
+        assert_eq!(self.warehouse_get(new_pos), Some(Block::Empty));
 
         if sim_only {
             return;
@@ -120,26 +135,26 @@ impl Input {
             .expect("warehouse get was out of bounds");
 
         match target_block {
-            b'.' => {
+            Block::Empty => {
                 self.raw_move_block(pos, target_pos, sim_only);
             }
-            b'#' => return None,
-            b'@' => panic!("you're not supposed to move a robot"),
-            b'O' => {
+            Block::Wall => return None,
+            Block::Robot => panic!("you're not supposed to move a robot"),
+            Block::Box => {
                 self.move_block(target_pos, dir, sim_only)?;
                 self.raw_move_block(pos, target_pos, sim_only);
             }
-            b'[' | b']' => {
+            Block::LBox | Block::RBox => {
                 if dir == directions::E || dir == directions::W {
                     // treat this like part1
                     self.move_block(target_pos, dir, sim_only)?;
                     self.raw_move_block(pos, target_pos, sim_only);
                 } else {
                     // Get the direction and expected_linked_block based on the target_block we're looking for
-                    let (direction, expected_linked_block) = if target_block == b'[' {
-                        (directions::E, b']')
+                    let (direction, expected_linked_block) = if target_block == Block::LBox {
+                        (directions::E, Block::RBox)
                     } else {
-                        (directions::W, b'[')
+                        (directions::W, Block::LBox)
                     };
 
                     // get the linked_block and linked_block_pos of our Box
@@ -164,7 +179,6 @@ impl Input {
                     }
                 }
             }
-            _ => panic!("unknown block"),
         }
         Some(target_pos)
     }
@@ -180,7 +194,8 @@ pub fn generator(input: &str) -> Input {
         if let Some(c) = line.bytes().position(|b| b == b'@') {
             robot_pos = (r, c);
         }
-        wh.push(line.to_string().into_bytes());
+        // SAFETY: Our Block enum contains all possible input bytes
+        wh.push(unsafe { transmute::<Vec<u8>, Vec<Block>>(line.to_string().into_bytes()) });
     }
 
     let dim = (wh.len(), wh[0].len());
@@ -222,7 +237,7 @@ pub fn part1(inputs: &Input) -> usize {
     }
 
     // println!("{}\n", inputs);
-    inputs.calculate_gps(b'O')
+    inputs.calculate_gps(Block::Box)
 }
 
 #[aoc(day15, part2)]
@@ -242,7 +257,7 @@ pub fn part2(inputs: &Input) -> usize {
     }
 
     // println!("{}\n", inputs);
-    inputs.calculate_gps(b'[')
+    inputs.calculate_gps(Block::LBox)
 }
 
 #[cfg(test)]
